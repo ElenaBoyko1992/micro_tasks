198232cccaec41c53951ebed5eb6345abe352c7e
ElenaBoyko1992 <Bionka@inbox.ru>
2022-08-17 16:33:02 +0300

    -

---
+++ 17.08.2022_Mon-Level_1-Homework_useState/src/App.tsx
@@ -0,0 +1,27 @@
+import React, {MouseEvent, useState} from 'react';
+import './App.css';
+
+
+function App() {
+
+    let [a, setA] = useState(1);
+    const onClickHandler = () => {
+        setA(++a);
+        console.log(a)
+    }
+
+    const onClickHandler2 = () => {
+        setA(0);
+        console.log(a)
+    }
+
+    return (
+        <div className={"App"}>
+            <h1>{a}</h1>
+            <button onClick={onClickHandler}>number</button>
+            <button onClick={onClickHandler2}>0</button>
+        </div>
+    )
+}
+
+export default App;
