b9e87084223940c82a2638f22ba041ca320c7f21
ElenaBoyko1992 <Bionka@inbox.ru>
2022-08-24 11:53:40 +0300

    23.08.22

---
+++ 23.08.2022_Mon-Level_2-Homework_input/src/App.tsx
@@ -0,0 +1,48 @@
+import React, {useState} from 'react';
+import './App.css';
+import {FullInput} from "./components/FullInput";
+import {Input} from "./components/Input";
+import {Button} from "./components/Button";
+
+function App() {
+    let [message, setMessage] = useState([
+        {message: 'message1'},
+        {message: 'message2'},
+        {message: 'message3'},
+
+    ])
+
+    let [title, setTitle] = useState('')
+
+    const addMessage = (title: string) => {
+        let newMessage = {message: title}
+        setMessage([newMessage, ...message])
+
+    }
+    const callBackButtonHandler = () => {
+        addMessage(title);
+        setTitle('')
+    }
+    return (
+        <div className="App">
+            {/*<FullInput
+                addMessage={addMessage}
+            />*/}
+            <Input
+                title={title}
+                setTitle={setTitle}
+            />
+            <Button name={'+'}
+                    callBack={callBackButtonHandler}
+            />
+
+            {message.map((el: { message: string }, index: number) => {
+                return (
+                    <div key={index}>{el.message}</div>
+                )
+            })}
+        </div>
+    );
+}
+
+export default App;
